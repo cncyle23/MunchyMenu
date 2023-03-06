3d698e3c6c9ca284c4f11edb7245e84ce62a4459
unknown <you@example.com>
2023-03-06 14:47:51 +0800

    MunchyMenu v1

---
+++ User app and web/web/firebase-messaging-sw.js
@@ -0,0 +1,49 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
+
+/*firebase.initializeApp({
+  apiKey: "AIzaSyDFN-73p8zKVZbA0i5DtO215XzAb-xuGSE",
+  authDomain: "ammart-8885e.firebaseapp.com",
+  projectId: "ammart-8885e",
+  storageBucket: "ammart-8885e.appspot.com",
+  messagingSenderId: "1000163153346",
+  appId: "1:1000163153346:web:4f702a4b5adbd5c906b25b",
+  databaseURL: "...",
+});*/
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBZoQLjheVdTY2nacSjOnziywBIwdV6fT0",
+    authDomain: "munchymenu-7f313.firebaseapp.com",
+    projectId: "munchymenu-7f313",
+    storageBucket: "munchymenu-7f313.appspot.com",
+    messagingSenderId: "509162340378",
+    appId: "1:509162340378:web:6effba6c3466ae183091a3"
+  databaseURL: "...",
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    const promiseChain = clients
+        .matchAll({
+            type: "window",
+            includeUncontrolled: true
+        })
+        .then(windowClients => {
+            for (let i = 0; i < windowClients.length; i++) {
+                const windowClient = windowClients[i];
+                windowClient.postMessage(payload);
+            }
+        })
+        .then(() => {
+            const title = payload.notification.title;
+            const options = {
+                body: payload.notification.score
+              };
+            return registration.showNotification(title, options);
+        });
+    return promiseChain;
+});
+self.addEventListener('notificationclick', function (event) {
+    console.log('notification received: ', event)
+});
